1bb631e279eec19a03a3042588b90ed3628b94de
shoaib63 <mirakishoaib@gmail.com>
2024-11-06 23:25:24 +0330

    End Of 15

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,40 +1,46 @@
 import { Link, useNavigate } from 'react-router-dom';
-import {createNewEvent} from '../../util/http.js';
+import { createNewEvent } from '../../util/http.js';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { useMutation } from '@tanstack/react-query';
-import  ErrorBlock  from '../UI/ErrorBlock.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { queryClient } from '../../util/http.js';
+
 
 
 export default function NewEvent() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
-  const { mutate , isPending, isError, error} = useMutation({
-    mutationFn: createNewEvent, 
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    }
   });
   function handleSubmit(formData) {
-    mutate({event: formData});
+    mutate({ event: formData });
   }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        {isPending && "Submitting..." }
+        {isPending && "Submitting..."}
         {!isPending && (
           <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
         )}
-        
+
       </EventForm>
-      {isError && <ErrorBlock title= "Failed to create event"
-         message={error.info?.message || 'Failed to create event. Please check your inputs and try again later.'} /> }
+      {isError && <ErrorBlock title="Failed to create event"
+        message={error.info?.message || 'Failed to create event. Please check your inputs and try again later.'} />}
     </Modal>
   );
 }
